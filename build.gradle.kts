a1de378762fada365e3dbeb88dc006d0754eaf55
sntuew_tort <sntuew@gmail.com>
2026-04-18 12:25:53 +0300

    update api

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,10 +12,12 @@ repositories {
 
 dependencies {
     testImplementation(kotlin("test"))
+    implementation("io.ktor:ktor-server-core:2.3.7")
     implementation("io.ktor:ktor-server-netty:2.3.7")
     implementation("io.ktor:ktor-server-content-negotiation:2.3.7")
     implementation("io.ktor:ktor-serialization-gson:2.3.7")
     implementation("org.postgresql:postgresql:42.7.1")
+    implementation("org.mindrot:jbcrypt:0.4")
 }
 
 kotlin {
